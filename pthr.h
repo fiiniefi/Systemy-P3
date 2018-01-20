ba57eab0fca10aa42885b2ea32927ed1b0ac71df
fiiniefi <marcin.drewniak1@vp.pl>
2018-01-20 02:21:16 +0100

    binsem completed, countsem added

--- pthr.h
+++ pthr.h
@@ -11,7 +11,6 @@ class Semaphore
 {
 protected:
     int signal;
-    std::queue<ucontext_t *> wthr;
 public:
     virtual void wake_up() = 0;
     virtual void wait() = 0;
@@ -26,5 +25,27 @@ public:
     {
         signal = 1;
     }
+    void wait()
+    {
+        while (signal == 0);
+        signal = 0;
+    }
+};
 
+class Countsem : public Semaphore
+{
+private:
+    int maxthr;
+public:
+    Countsem(int maxthr) : Semaphore(maxthr), maxthr(maxthr) {}
+    void wake_up()
+    {
+        if (signal < maxthr)
+            signal++;
+    }
+    void wait()
+    {
+        while (signal == 0);
+        signal--;
+    }
 };
